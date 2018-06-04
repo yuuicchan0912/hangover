ea28bb511ea43b6360a3b00720dd1bc9f98c0b84
kikutetsu <kikutetsu.5555@gmail.com>
2018-06-04 22:38:57 +0900

    set react-rails

---
+++ app/assets/components/FirstTest.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const FirstTest = ({}) => {
+  return (
+    <ul>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+    </ul>
+  );
+}
+
+export default FirstTest
---
+++ app/assets/packs/application.js
@@ -0,0 +1,14 @@
+require.context('images', true, /\.(png|jpg|jpeg|svg)$/)
+import "babel-polyfill"
+import '../javascripts/application'
+import '../stylesheets/application'
+
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
+
+import * as Rails from 'rails-ujs'
+Rails.start();
+
+console.log('Hello World from Webpacker');
